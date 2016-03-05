08f1f61eef2a19f6e9be116a74fa9f2255e0803b
Naomi Dushay <ndushay@stanford.edu>
2016-03-05 01:40:50 -0800

    slight tweaks to existing tests

--- spec/purl_to_sw_spec.rb
+++ spec/purl_to_sw_spec.rb
@@ -1,27 +1,26 @@
 require 'spec_helper'
 
-describe "All Purl objects released to SearchWorks" do
+describe "Purl objects released to SearchWorks" do
 
   it "SearchWorks has access_facet 'Online' for every Digital Collection object" do
-    solr_params = { 'fq' => ["collection_type:\"Digital Collection\"", "-access_facet:Online"] }
+    solr_params = { 'fq' => ['collection_type:"Digital Collection"', '-access_facet:Online'] }
     resp = solr_resp_doc_ids_only(SolrConns.sw, solr_params)
-    expect(resp).not_to include("id" => /.+/)  # get ids of errant records
+    expect(resp).not_to include("id" => /.+/)  # gets ids of errant records
   end
 
   it "SearchWorks has building_facet 'Stanford Digital Repoistory' for every Digital Collection object" do
-    solr_params = { 'fq' => ["collection_type:\"Digital Collection\"", "-building_facet:\"Stanford Digital Repository\""] }
+    solr_params = { 'fq' => ['collection_type:"Digital Collection"', '-building_facet:"Stanford Digital Repository"'] }
     resp = solr_resp_doc_ids_only(SolrConns.sw, solr_params)
-    expect(resp).not_to include("id" => /.+/)  # get ids of errant records
+    expect(resp).not_to include("id" => /.+/)  # gets ids of errant records
   end
 
-  it "correct number of digital collections" do
+  it "number of released Purl digital collections agrees with SW" do
     purl_solr_params = { 'fq' => ['true_releases_ssim:Searchworks', 'identityMetadata_objectType_t:collection'] }
     purl_sw_colls_resp = solr_resp_doc_ids_only(SolrConns.purl, purl_solr_params)
-    sw_dig_colls_resp = solr_resp_doc_ids_only(SolrConns.sw, 'fq' => "collection_type:\"Digital Collection\"")
+    sw_dig_colls_resp = solr_resp_doc_ids_only(SolrConns.sw, 'fq' => 'collection_type:"Digital Collection"')
     expect(sw_dig_colls_resp.size).to be <= purl_sw_colls_resp.size
 
-    # TODO:  when dispatcher service automagically tells SW to index newly released collection to SW prod,
-    # these should match.
+    # TODO:  when dispatcher service automagically indexes newly released collections to SW prod, these should match.
     # as of 3/1/2016, dispatcher service has not been deployed, and true_releases_ssim means objects
     # *may* (or may not) have been manually deployed to SW perpetual preview
     # TODO: using pending below causes jenkinsqa build to fail
@@ -32,21 +31,22 @@ describe "All Purl objects released to SearchWorks" do
     purl_solr_params = { 'fq' => ['true_releases_ssim:Searchworks', 'identityMetadata_objectType_t:collection'],
       'fl' => 'id,catkey_tsi', 'rows' => '300' }
     purl_sw_colls_resp = solr_response(SolrConns.purl, purl_solr_params)
-    purl_sw_colls_resp['response']['docs'].each do |doc|
+    purl_sw_colls_resp['response']['docs'].each do |purl_solr_doc|
       sw_doc_id =
-        if doc['catkey_tsi']
-          doc['catkey_tsi']
+        if purl_solr_doc['catkey_tsi']
+          purl_solr_doc['catkey_tsi']
         else
-          doc['id'].split('druid:').last
+          bare_druid(purl_solr_doc['id'])
         end
-      sw_doc_resp = solr_resp_single_doc(SolrConns.sw, sw_doc_id)
-      sw_doc = sw_doc_resp['response']['docs'].first
+      sw_doc = solr_resp_single_doc(SolrConns.sw, sw_doc_id)['response']['docs'].first
       # TODO: use expect below after dispatcher service automagically tells SW to index newly released colls
       # expect(sw_doc['collection_type']).to eq ['Digital Collection'] if sw_doc
-      puts "ALERT: expected #{sw_doc_id} to be a Digital Collection!" unless sw_doc && sw_doc['collection_type'] == ['Digital Collection']
+      unless sw_doc && sw_doc['collection_type'] == ['Digital Collection']
+        puts "ALERT: expected #{sw_doc_id} to be a Digital Collection!"
+      end
     end
     # TODO: using pending below causes jenkinsqa build to fail
-    skip('when dispatcher service sends true releases to SW prod, we should use expect statement but output ids of probs')
+    skip('when dispatcher service sends true releases to SW prod, we should use expect statement') # and output ids of probs
   end
 
   it "Hydrus single items from Purl are single digital repo items without coll in SW" do
